d7923bee8ae334074517596207b831bae5f00751
ThaiHa279 <levancuter@gmail.com>
2023-04-02 10:11:58 +0700

    Tao trang cap nhat lien he

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import router from "./router";
+
+library.add(fas, far);
+
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount("#app");
